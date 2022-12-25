0320b7f252fa51e36b893b635cb9b52221fa9ae1
Ioannis Brant-Ioannidis <giannisbrant@gmail.com>
2022-12-25 14:51:22 +0200

    Adding UTs

--- Kata - Data Munging/spec/file-service-spec.ts
+++ Kata - Data Munging/spec/file-service-spec.ts
@@ -2,10 +2,17 @@ import { FileService } from "../Services/FileService";
 
 describe("Test suite for array sanitization", function () {
     let fileService: FileService = new FileService();
+    let rawData: string[] = fileService.readData("./Files/weather.dat");
 
-    it("returns an empty array if the passed array contains only whitespace strings", function () {
+    it("fails if reading fails", function () {
+        let isDataArrayEmpty = rawData.length === 0;
+        expect(isDataArrayEmpty).toBe(false);
     });
 
     it("returns an array without whitespaces", function () {
+        let dataMap: Map<number, string[]> = fileService.splitLinesBasedOnWhitespaces(rawData);
+        let isMapEmpty: boolean = dataMap.size === 0;
+
+        expect(isMapEmpty).toBe(false);
     });
 });
