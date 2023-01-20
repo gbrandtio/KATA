f98dc9981c84e39f9c48b1ffac5643355ae0c8ce
Ioannis Brant-Ioannidis <giannisbrant@gmail.com>
2023-01-21 02:11:18 +0400

    Differentiating validation
    Differentiating validation rules to make the example a little bit more interesting

--- Kata - Refactor complex get/main.ts
+++ Kata - Refactor complex get/main.ts
@@ -1,7 +1,11 @@
+import { BaseHandler } from './Handlers/BaseHandler';
 import { Customer } from './Models/Customer';
 
 export class Main {
+    public mainCustomerValidationHandler!: BaseHandler; 
+    public additionalCustomerValidationHandler!: boolean;
     private customer: Customer;
+    
     constructor(customer: Customer) {
         this.customer = customer;
     }
@@ -19,11 +23,12 @@ export class Main {
 
         if (this.customer.additionalCustomer) {
             if (this.customer.company) {
-                customerIsValid = false;
-            } else {
                 customerIsValid = true;
+            } else {
+                customerIsValid = false;
             }
         }
+
         return customerIsValid;
     }
 }
