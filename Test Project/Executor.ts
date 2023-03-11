31e388c9c00ba130922bc5c60368c553ca5ec3c5
Ioannis Brant-Ioannidis <giannisbrant@gmail.com>
2023-03-12 01:12:08 +0400

    Removing test project

--- Test Project/Executor.ts
+++
@@ -1,9 +0,0 @@
-
-
-export class Executor<T> {
-    public execute(unformattedUsername: T): T {
-        let formattedUsername: T = unformattedUsername;
-
-        return formattedUsername;
-    }
-}
